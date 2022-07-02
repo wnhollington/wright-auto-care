f9dbd9d03f38af7e3f62c99a1931b52a536e663b
wnhollington <wnhollington@gmail.com>
2022-07-02 07:49:52 -0600

    revise hero

--- src/components/index/hero.js
+++ src/components/index/hero.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { useSiteMetadata } from "../../utilities/use-site-metadata"
 import { Link } from "gatsby"
 import { useInView } from "react-intersection-observer"
+import { Container } from "react-bootstrap"
 
 const Hero = () => {
     const siteMetaData = useSiteMetadata()
@@ -11,17 +12,14 @@ const Hero = () => {
     })
     return (
         <section id="hero" className="hero" ref={ref}>
-            <div className="hero-container">
-                <div className="carousel-item active">
-                    <div className="carousel-container">
-                    <div className={`carousel-content container animated animatedFadeInUp ${inView ? ' fadeInUp' : null}`}>
+            <div className="carousel-item active">
+                <div className="carousel-container">
+                    <Container className={`carousel-content container animated animatedFadeInUp ${inView ? ' fadeInUp' : null}`}>
                         <h2>{siteMetaData.title}</h2>
                         <p>Professional and reliable mobile auto detailing at your door.</p>
                         <Link to="/schedule-service"className="btn-get-started text-white">Schedule Now</Link>
-                    </div>
-                    </div>
+                    </Container>
                 </div>
-
             </div>
         </section>
     )
